414c71f29affa75d8be8ad4611b6c68f8fd6dcae
Matt Schnee <i.am@matthew.engineer>
2020-06-16 10:15:12 -0700

    only  on /^feat:/

--- uprev/index.js
+++ uprev/index.js
@@ -22,9 +22,7 @@ Toolkit.run(async (tools) => {
   }
 
   let version = 'patch';
-  if (messages.map((message) => message.includes('BREAKING CHANGE') || message.includes('major')).includes(true)) {
-    version = 'major';
-  } else if (messages.map((message) => message.toLowerCase().startsWith('feat') || message.toLowerCase().includes('minor')).includes(true)) {
+  if (messages.map((message) => message.toLowerCase().startsWith('feat:')).includes(true)) {
     version = 'minor';
   }
 
